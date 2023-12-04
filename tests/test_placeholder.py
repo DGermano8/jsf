52dd3000f4f820fe53cec45eb4beb9143e9e4fc1
Alex Zarebski <azarebski@6200l-214217-l.mobility.unimelb.net.au>
2023-12-04 17:35:57 +1100

    include readme and placeholder test

---
+++ tests/test_placeholder.py
@@ -0,0 +1,9 @@
+import unittest
+import jsf
+
+class TestPlaceholderFunction(unittest.TestCase):
+    def test_output(self):
+        self.assertEqual(1, 1)
+
+if __name__ == '__main__':
+    unittest.main()
