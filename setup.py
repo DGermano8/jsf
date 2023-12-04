fc5395e1aef5a288404052bc50eebf1697e76a06
Alex Zarebski <azarebski@6200l-214217-l.mobility.unimelb.net.au>
2023-12-04 17:29:00 +1100

    copy the jsf implementation across

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jsf',
+    version='0.1',
+    packages=find_packages(),
+    description='Implementation of the jump-switch-flow simulator',
+    author='Domenic Germano',
+    author_email='germanod@student.unimelb.edu.au',
+    url='https://github.com/DGermano8/jazzy-shrill-fart',
+)
