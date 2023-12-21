def5c994e89624856975675de630894694a516ce
Alex Zarebski <azarebski@6200l-214217-l.mobility.unimelb.net.au>
2023-12-21 13:48:25 +1100

    use more descriptive names for the variables that track frozen and flowing reactions

--- jsf/__init__.py
+++ jsf/__init__.py
@@ -78,15 +78,21 @@ def JumpSwitchFlowSimulator(x0, rates, stoich, t_max, options):
     ReactComp = [[value != 0 for value in row] for row in nuReactant]
     compartInNu = [[value != 0 for value in row] for row in MatrixPlusAB(NuComp,ReactComp)]
 
-    discCompartment = [False]*nRates
+    # NOTE the variable `frozenReaction` is used to track which of the
+    # potential reactions are frozen (i.e. only jumping not flowing).
+    # This variable used to be called `discCompartment`. In a few
+    # loops where there used to be a `compartIdx` iterator, these have
+    # been changed to `reactionIdx` since these are looping over the
+    # possible reactions not the compartments.
+    frozenReaction = [False]*nRates
     for idx in range(nCompartments):
-        for compartIdx in range(nRates):
+        for reactionIdx in range(nRates):
             if not EnforceDo[idx]:
-                if DoDisc[idx] and compartInNu[compartIdx][idx]:
-                    discCompartment[compartIdx] = True
+                if DoDisc[idx] and compartInNu[reactionIdx][idx]:
+                    frozenReaction[reactionIdx] = True
             else:
-                if DoDisc[idx] and compartInNu[compartIdx][idx]:
-                    discCompartment[compartIdx] = True
+                if DoDisc[idx] and compartInNu[reactionIdx][idx]:
+                    frozenReaction[reactionIdx] = True
 
     # initialise discrete sum compartments
     integralOfFiringTimes = [0]*nRates
@@ -112,7 +118,7 @@ def JumpSwitchFlowSimulator(x0, rates, stoich, t_max, options):
     # The `newlyDiscCompIndex` variable is used to track which
     # compartment has recently switched to being discrete. It was
     # previously called NewDiscCompartmemt, not to be confused with
-    # NewdiscCompartment or discCompartment which is a list of
+    # NewfrozenReaction or frozenReaction which is a list of
     # booleans describing which reactions are frozen.
     newlyDiscCompIndex = None
     correctInteger = 0
@@ -125,10 +131,10 @@ def JumpSwitchFlowSimulator(x0, rates, stoich, t_max, options):
         Props = rates(Xprev, ContT)
 
         # Perform the Forward Euler Step
-        dXdt = ComputedXdt(Xprev, Props, nu, discCompartment, nCompartments)
+        dXdt = ComputedXdt(Xprev, Props, nu, frozenReaction, nCompartments)
 
         # check if any states change in this step
-        Dtau, correctInteger, DoDisc, discCompartment, NewDoDisc, NewdiscCompartment, newlyDiscCompIndex = UpdateCompartmentRegime(dt, Xprev, Dtau, dXdt, Props, nu, SwitchingThreshold, DoDisc, EnforceDo, discCompartment, compartInNu, nCompartments,nRates)
+        Dtau, correctInteger, DoDisc, frozenReaction, NewDoDisc, NewfrozenReaction, newlyDiscCompIndex = UpdateCompartmentRegime(dt, Xprev, Dtau, dXdt, Props, nu, SwitchingThreshold, DoDisc, EnforceDo, frozenReaction, compartInNu, nCompartments,nRates)
 
         # Only apply the forward Euler step if the compartment is continuous.
         Xcurr = [X[i][iters] + (0 if DoDisc[i] else Dtau * dXdt[i]) for i in range(nCompartments)]
@@ -136,8 +142,8 @@ def JumpSwitchFlowSimulator(x0, rates, stoich, t_max, options):
         # Update the discrete compartments, if a state has just become discrete
         OriginalDoDisc = DoDisc[:]
         if correctInteger == 1:
-            NewDoDisc, NewdiscCompartment = IsDiscrete(Xprev, SwitchingThreshold, DoDisc, EnforceDo, discCompartment, compartInNu, nCompartments,nRates)
-            discCompartment = NewdiscCompartment[:]
+            NewDoDisc, NewfrozenReaction = IsDiscrete(Xprev, SwitchingThreshold, DoDisc, EnforceDo, frozenReaction, compartInNu, nCompartments,nRates)
+            frozenReaction = NewfrozenReaction[:]
             DoDisc = NewDoDisc[:]
 
         # Perform the Stochastic Loop
@@ -155,7 +161,7 @@ def JumpSwitchFlowSimulator(x0, rates, stoich, t_max, options):
                 Props = rates(Xcurr, AbsT)
 
             integralStep = ComputeIntegralOfFiringTimes(Dtau, Props, rates, Xprev, Xcurr, AbsT)
-            integralOfFiringTimes = ArrayPlusAB(integralOfFiringTimes,ArrayMultiplyAB(integralStep, discCompartment))
+            integralOfFiringTimes = ArrayPlusAB(integralOfFiringTimes,ArrayMultiplyAB(integralStep, frozenReaction))
 
             # If any of the components have just become discrete, we need to update the integralOfFiringTimes and randTimes
             if correctInteger == 1:
@@ -163,13 +169,13 @@ def JumpSwitchFlowSimulator(x0, rates, stoich, t_max, options):
                     if newlyDiscCompIndex==ii and not EnforceDo[ii]:
                         for jj in range(nRates):
                             if compartInNu[jj][ii]:
-                                discCompartment[jj] = True
+                                frozenReaction[jj] = True
                                 integralOfFiringTimes[jj] = 0.0
                                 randTimes[jj] = random.random()
 
             firedReactions = [
                         ((0 > (rand - (1 - math.exp(-integral)))) and disc)
-                        for rand, integral, disc in zip(randTimes, integralOfFiringTimes, discCompartment)
+                        for rand, integral, disc in zip(randTimes, integralOfFiringTimes, frozenReaction)
                     ]
 
             if NNZ(firedReactions) > 0:
@@ -179,7 +185,7 @@ def JumpSwitchFlowSimulator(x0, rates, stoich, t_max, options):
                 if NNZ(tauArray) > 0:
 
                     # Update the discrete compartments
-                    Xcurr, Xprev, integralOfFiringTimes, integralStep, randTimes, TimePassed, AbsT, DtauMin = ImplementFiredReaction(tauArray ,integralOfFiringTimes,randTimes,Props,rates,integralStep,TimePassed, AbsT, X, iters, nu, dXdt, OriginalDoDisc, discCompartment)
+                    Xcurr, Xprev, integralOfFiringTimes, integralStep, randTimes, TimePassed, AbsT, DtauMin = ImplementFiredReaction(tauArray ,integralOfFiringTimes,randTimes,Props,rates,integralStep,TimePassed, AbsT, X, iters, nu, dXdt, OriginalDoDisc, frozenReaction)
 
                     iters = iters + 1
                     for i in range(nCompartments):
@@ -210,7 +216,7 @@ def JumpSwitchFlowSimulator(x0, rates, stoich, t_max, options):
 
             for jj in range(nRates):
                 if compartInNu[jj][pos]:
-                    discCompartment[jj] = True
+                    frozenReaction[jj] = True
                     integralOfFiringTimes[jj] = 0.0
                     randTimes[jj] = random.random()
             newlyDiscCompIndex = None
@@ -227,7 +233,7 @@ def ComputeFiringTimes(firedReactions,integralOfFiringTimes,randTimes,Props,dt,n
 
     return tauArray
 
-def ImplementFiredReaction(tauArray ,integralOfFiringTimes,randTimes,Props,rates,integralStep,TimePassed, AbsT, X, iters, nu, dXdt, OriginalDoDisc,discCompartment):
+def ImplementFiredReaction(tauArray ,integralOfFiringTimes,randTimes,Props,rates,integralStep,TimePassed, AbsT, X, iters, nu, dXdt, OriginalDoDisc,frozenReaction):
     tauArray = [float('inf') if tau == 0.0 else tau for tau in tauArray]
 
     DtauMin = min(tauArray)
@@ -240,9 +246,9 @@ def ImplementFiredReaction(tauArray ,integralOfFiringTimes,randTimes,Props,rates
     Xcurr = [X[i][iters] + nu[pos][i] + (0 if OriginalDoDisc[i] else DtauMin * dXdt[i]) for i in range(nCompartments)]
     Xprev = [X[i][iters] for i in range(nCompartments)]
 
-    integralOfFiringTimes = [integral - step*disc for integral, step, disc in zip(integralOfFiringTimes, integralStep, discCompartment)]
+    integralOfFiringTimes = [integral - step*disc for integral, step, disc in zip(integralOfFiringTimes, integralStep, frozenReaction)]
     integralStep = ComputeIntegralOfFiringTimes(DtauMin, Props, rates, Xprev, Xcurr, AbsT)
-    integralOfFiringTimes = [integral + step*disc for integral, step, disc in zip(integralOfFiringTimes, integralStep, discCompartment)]
+    integralOfFiringTimes = [integral + step*disc for integral, step, disc in zip(integralOfFiringTimes, integralStep, frozenReaction)]
 
     integralOfFiringTimes[pos] = 0.0
     randTimes[pos] = random.random()
@@ -255,18 +261,18 @@ def ComputeIntegralOfFiringTimes(Dtau, Props, rates, Xprev, Xcurr, AbsT):
     return integralStep
 
 
-def ComputedXdt(Xprev, Props, nu, discCompartment, nCompartments):
+def ComputedXdt(Xprev, Props, nu, frozenReaction, nCompartments):
     """
     Compute the derivative of the state vector at time t for the
     continuous compartments by summing the contributions from each
     reaction.
     """
-    return [sum(0 if discCompartment[i] else Props[i] * nu[i][j] for i in range(len(Props))) for j in range(nCompartments)]
+    return [sum(0 if frozenReaction[i] else Props[i] * nu[i][j] for i in range(len(Props))) for j in range(nCompartments)]
 
 
-def UpdateCompartmentRegime(dt, Xprev, Dtau, dXdt, Props, nu, SwitchingThreshold, DoDisc, EnforceDo, discCompartment, compartInNu, nCompartments,nRates):
+def UpdateCompartmentRegime(dt, Xprev, Dtau, dXdt, Props, nu, SwitchingThreshold, DoDisc, EnforceDo, frozenReaction, compartInNu, nCompartments,nRates):
     # check if any states change in this step
-    NewDoDisc, NewdiscCompartment = IsDiscrete(Xprev, SwitchingThreshold, DoDisc, EnforceDo, discCompartment, compartInNu, nCompartments,nRates)
+    NewDoDisc, NewfrozenReaction = IsDiscrete(Xprev, SwitchingThreshold, DoDisc, EnforceDo, frozenReaction, compartInNu, nCompartments,nRates)
 
     correctInteger = 0
     newlyDiscCompIndex = None
@@ -294,16 +300,16 @@ def UpdateCompartmentRegime(dt, Xprev, Dtau, dXdt, Props, nu, SwitchingThreshold
                     newlyDiscCompIndex = pos
                     correctInteger = 1
         else:
-            discCompartment = NewdiscCompartment
+            frozenReaction = NewfrozenReaction
             DoDisc = NewDoDisc
             # correctInteer = 1
     else:
-        discCompartment = NewdiscCompartment
+        frozenReaction = NewfrozenReaction
         DoDisc = NewDoDisc
 
-    return Dtau, correctInteger, DoDisc, discCompartment, NewDoDisc, NewdiscCompartment, newlyDiscCompIndex
+    return Dtau, correctInteger, DoDisc, frozenReaction, NewDoDisc, NewfrozenReaction, newlyDiscCompIndex
 
-def IsDiscrete(X, SwitchingThreshold, DoDisc, EnforceDo, discCompartment, compartInNu, nCompartments, nRates):
+def IsDiscrete(X, SwitchingThreshold, DoDisc, EnforceDo, frozenReaction, compartInNu, nCompartments, nRates):
     # Check if any compartments should be switched to continuous
     DoDiscTmp = [x <= threshold for x, threshold in zip(X, SwitchingThreshold)]
 
@@ -314,21 +320,21 @@ def IsDiscrete(X, SwitchingThreshold, DoDisc, EnforceDo, discCompartment, compar
     are_equal = [DoDiscTmp[idx] == DoDisc[idx] for idx in range(nCompartments)]
 
     if all(are_equal):
-        discCompartmentTmp = discCompartment.copy()
+        frozenReactionTmp = frozenReaction.copy()
     else:
-        discCompartmentTmp = [False] * nRates
+        frozenReactionTmp = [False] * nRates
 
         for idx in range(nCompartments):
-            for compartIdx in range(nRates):
+            for reactionIdx in range(nRates):
                 if not EnforceDo[idx]:
-                    if DoDiscTmp[idx] and compartInNu[compartIdx][idx] == 1:
-                        discCompartmentTmp[compartIdx] = True
+                    if DoDiscTmp[idx] and compartInNu[reactionIdx][idx] == 1:
+                        frozenReactionTmp[reactionIdx] = True
                 else:
-                    if DoDisc[idx] and compartInNu[compartIdx][idx] == 1:
-                        discCompartmentTmp[compartIdx] = True
+                    if DoDisc[idx] and compartInNu[reactionIdx][idx] == 1:
+                        frozenReactionTmp[reactionIdx] = True
 
 
-    return DoDiscTmp, discCompartmentTmp
+    return DoDiscTmp, frozenReactionTmp
 
 # these are helper functions to make it readable
 def ArraySubtractAB(ArrayA, ArrayB):
