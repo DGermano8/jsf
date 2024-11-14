fa0c665fe7fa642d4cd370410bbcb9f2a02d4734
aezarebski <aezarebski@gmail.com>
2024-11-14 14:22:08 +1100

    add a type signature to make it easier to read

--- jsf/sbml.py
+++ jsf/sbml.py
@@ -4,7 +4,9 @@ from typing import Any, Callable, Dict, List, Tuple
 from jsf.types import Time, SystemState, CompartmentValue, Trajectory
 
 
-def read_sbml(sbml_xml: str) -> Tuple[SystemState, Callable[[SystemState, Time], List[float]], Dict[str, Any]]:
+def read_sbml(
+    sbml_xml: str,
+) -> Tuple[SystemState, Callable[[SystemState, Time], List[float]], Dict[str, Any]]:
     reader = libsbml.SBMLReader()
     doc = reader.readSBML(sbml_xml)
     model = doc.getModel()
